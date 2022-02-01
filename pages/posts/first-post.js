0277d0ce6966353f1929aa7b56d1351e89208838
nao <nao_h13@hotmail.com>
2022-02-01 22:46:20 +0900

    style の適用

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,27 +1,28 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
-      <title>First Post</title>
-    </Head>
-    <Script
-      src="https://connect.facebook.net/en_US/sdk.js"
-      strategy="lazyOnload"
-      onLoad={ () => 
-        console.log(`script loaded corrently, window.FB has been populated`)
-      }
-      />
-    <h1>First Post</h1>
-    <h2>
-      <img src="/images/profile.jpg" alter="Your Name"></img><br/>
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
-    </h2>
-    </>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={ () => 
+          console.log(`script loaded corrently, window.FB has been populated`)
+        }
+        />
+      <h1>First Post</h1>
+      <h2>
+        <img src="/images/profile.jpg" alter="Your Name"></img><br/>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
   ) 
 }
