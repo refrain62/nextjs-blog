af5386bc53e8ecc66a9040d6f346f19b0d7c90c5
nao <nao_h13@hotmail.com>
2022-02-01 23:32:49 +0900

    グローバルレイアウトの実装

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -9,20 +9,7 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={ () => 
-          console.log(`script loaded corrently, window.FB has been populated`)
-        }
-        />
       <h1>First Post</h1>
-      <h2>
-        <img src="/images/profile.jpg" alter="Your Name"></img><br/>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
     </Layout>
   ) 
 }
