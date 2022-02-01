a28c0e6d53b16f512fe5a66d1d2a17c5919baa8c
nao <nao_h13@hotmail.com>
2022-02-01 23:48:34 +0900

    classnames ライブラリを使用した　クラスの切替実装例

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Alert from '../components/alert'
 
 export default function Home() {
   return (
@@ -15,6 +16,9 @@ export default function Home() {
           (This is a sample website - you'll be bulding a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a> .)
         </p>
+
+        <Alert type="success">this alert success content.</Alert>
+        <Alert type="error">this alert error content.</Alert>
       </section>
 
     </Layout>
