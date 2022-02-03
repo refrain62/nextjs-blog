73ee0ae117a0cd0fb47fa581b674740de1416c12
nao <nao_h13@hotmail.com>
2022-02-03 23:41:39 +0900

    Jest(Rust)の追加

--- README.md
+++ README.md
@@ -94,3 +94,36 @@ npm run dev
 ## API 実装
 
 ・ http://localhost:3000/api/hello
+
+## Jest コンパイラを Babel から Rust に
+
+以下の公式内容を参考に  
+https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
+
+```
+npm install --save-dev jest @testing-library/react @testing-library/jest-dom
+```
+
+jest.config.js に設定追加
+
+```
+// jest.config.js
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  // Add more setup options before each test is run
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -28,45 +28,47 @@ export default function Home({
 }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
+      <>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
 
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you'll be bulding a site like this on{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a> .)
-        </p>
+        <section className={utilStyles.headingMd}>
+          <p>[Your Self Introduction]</p>
+          <p>
+            (This is a sample website - you'll be bulding a site like this on{' '}
+            <a href='https://nextjs.org/learn'>our Next.js tutorial</a> .)
+          </p>
 
-        <Alert type='success'>this alert success content.</Alert>
-        <Alert type='error'>this alert error content.</Alert>
-      </section>
+          <Alert type='success'>this alert success content.</Alert>
+          <Alert type='error'>this alert error content.</Alert>
+        </section>
 
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
 
-      <section>
-        <h2>API 実装</h2>
-        <p>Hello</p>
-        <p>
-          <a href='http://localhost:3000/api/hello'>http://localhost:3000/api/hello</a>
-        </p>
-      </section>
+        <section>
+          <h2>API 実装</h2>
+          <p>Hello</p>
+          <p>
+            <a href='http://localhost:3000/api/hello'>http://localhost:3000/api/hello</a>
+          </p>
+        </section>
+      </>
     </Layout>
   );
 }
